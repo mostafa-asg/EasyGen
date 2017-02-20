33dfa184e2a56aaed28f20e90cebc6c37bea5dd7
Mostafa Asgari <mostafa.asg@gmail.com>
2017-02-20 11:05:38 +0330

    capability to parse some basic syntax

---
+++ src/main/java/com/github/generator/parser/Parser.java
@@ -0,0 +1,8 @@
+package com.github.generator.parser;
+
+/**
+ * @author Mostafa Asgari
+ * @since 2/20/17
+ */
+public class Parser {
+}
---
+++ src/test/java/com/github/TestCase.java
@@ -0,0 +1,8 @@
+package com.github;
+
+/**
+ * @author Mostafa Asgari
+ * @since 2/20/17
+ */
+public class TestCase {
+}
